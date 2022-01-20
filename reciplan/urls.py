e7e8195c96a3f2cb6416e979a4662ff0b10171c6
jd1224 <Joshua.coe11@yahoo.com>
2022-01-20 18:37:25 -0500

    added authentication
    Added user signup/login/signout/password reset/authorization to the project. All templates are now functional and ready for HTML/CSS front-end styling.

--- reciplan/urls.py
+++ reciplan/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('authorized/', views.authorized, name='authorized'),
+    #add the paths for the builtin Django user functions
     path("accounts/", include("django.contrib.auth.urls")),
+    path("register/", views.register, name="register"),
 ]
