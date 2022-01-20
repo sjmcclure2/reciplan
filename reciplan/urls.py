a8462469a83977d72f87395a8e34e6a50a98020e
jd1224 <Joshua.coe11@yahoo.com>
2022-01-19 20:14:39 -0500

    added auth page
    added auth page

--- reciplan/urls.py
+++ reciplan/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('authorized/', views.authorized, name='authorized'),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
--- reciplan/views.py
+++ reciplan/views.py
@@ -4,3 +4,6 @@ from .models import Recipe
 def index(request):
     recipes = Recipe.objects.all()
     return render(request, 'reciplan/home.html', {'recipes':recipes})
+
+def authorized(request):
+    return render(request, 'reciplan/authorized.html')
