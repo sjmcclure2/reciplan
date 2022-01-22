f4509083603f1f325b9f657513d8b01ef24ece42
sjmcclure2 <sjmcclure2@gmail.com>
2022-01-22 12:33:06 -0600

    updated models.py

--- reciplan/models.py
+++ reciplan/models.py
@@ -1,9 +1,17 @@
+from tkinter import CASCADE
 from django.db import models
+from sqlalchemy import ForeignKey
 
 # Create your models here.
 class Recipe(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
-    ingredients = models.CharField(max_length=500)
     image = models.ImageField(upload_to="reciplan/images/")
     url = models.URLField(blank=True)
+
+
+class Ingredients(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=CASCADE)
+    name = models.CharField(max_length=50)
+    amt = models.IntegerField()
+    unit_of_measure = models.CharField(max_length=10)
