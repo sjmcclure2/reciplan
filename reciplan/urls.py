a95fa59b8aa642284f03aa134f971a229154d153
jd1224 <Joshua.coe11@yahoo.com>
2022-01-22 12:48:34 -0500

    Added search
    added search function, recipe display placeholder, and create recipe placeholder. using bootstrap for card type display and static side nav bar.

--- reciplan/urls.py
+++ reciplan/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('authorized/', views.authorized, name='authorized'),
+    path('create/', views.create_recipe, name='create'),
     #add the paths for the builtin Django user functions
     path("accounts/", include("django.contrib.auth.urls")),
     path("register/", views.register, name="register"),
+    path("search/", views.search, name="search"),
+    path("recipe/(?<name>\*)", views.detail, name="detail"),
 ]
--- reciplan/views.py
+++ reciplan/views.py
@@ -10,12 +10,12 @@ def index(request):
     return render(request, 'reciplan/home.html')
 
 @login_required
-def authorized(request):
+def create_recipe(request):
     #sample of a view that requires login based on the decorator
     #sample database call for all recipes passed
     #in the render request to the template
-    recipes = Recipe.objects.all()
-    return render(request, 'reciplan/authorized.html', {'recipes':recipes})
+    if request.method == 'GET':
+        return render(request, 'reciplan/create_recipe.html')
 
 #Registration function
 def register(request):
@@ -37,7 +37,7 @@ def register(request):
     #if request is POST attempt to create new user
     elif request.method == "POST":
         form = CustomUserCreationForm(request.POST)
-        print(form.errors)
+        #print(form.errors)
         form_errors = ""
         for i in form.errors:
             form_errors+=form.errors[i]
@@ -50,3 +50,22 @@ def register(request):
         #if user form fails redirect to new register with errors displayed
             request.session['errors'] = form_errors
             return redirect(reverse('register'))
+    
+def search(request):
+    #get the first 5 recipes from the DB
+    recipes = Recipe.objects.all()[0:5]
+    #if the method is POST take the form input
+    if request.method == "POST":
+        query_name = request.POST.get('name', None)
+        if query_name:
+            #query the DB for recipes with a title containing the search term
+            results = Recipe.objects.filter(title__contains=query_name)
+            #return the search template with the required variables for the display
+            return render(request, 'reciplan/search.html', {"results":results, "query":query_name, 'recipes':recipes})
+    #if there are no results display all available recipes in a list.
+    #these might look good as bootstrap cards, currently limited to 5 recipes but can increase
+    return render(request, 'reciplan/search.html', {'recipes':recipes})
+
+def detail(request, name):
+    results = Recipe.objects.get(title=name)
+    return render(request, 'reciplan/recipe_view.html', {'recipe':results})
