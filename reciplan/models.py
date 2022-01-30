9919696609801be7c02fa8fe3c8074662d115439
sjmcclure2 <sjmcclure2@gmail.com>
2022-01-29 18:26:15 -0600

    Models updated and conversions.py added

--- reciplan/models.py
+++ reciplan/models.py
@@ -1,18 +1,32 @@
 from django.db import models
+from django.http import cookie
+from conversions import to_cups
 
 class Recipe(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
     o_yield = models.IntegerField()
     directions = models.TextField()
-    image = models.ImageField(upload_to="reciplan/images/")
+    image = models.ImageField(upload_to="reciplan/images/", blank=True)
     url = models.URLField(blank=True)
 
 
 class Ingredients(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length = 50)
     amt = models.IntegerField()
-    unit_of_measure = models.CharField(max_length=10)
-    in_cart = models.BooleanField(default=False)
-
+    UOM = (
+        ('fl_oz', 'fl oz'),
+        ('fl_cups', 'fl cups'),
+        ('cups', 'cups'),
+        ('pints', 'pints'),
+        ('quarts', 'quarts'), 
+        ('gallons', 'gallons'), 
+        ('tsp', 'tsp'), 
+        ('Tbsp', 'Tbsp'),
+        ('ea', 'ea')
+    )
+    unit_of_measure = models.CharField(max_length=100, choices = UOM)
+    cup_amt = to_cups(Recipe.__getattribute__o_yield, unit_of_measure, amt)
+    
+  
