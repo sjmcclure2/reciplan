fcec76a1759359ad3f4a53a5de76f1cfacce2b1f
jd1224 <Joshua.coe11@yahoo.com>
2022-02-06 18:01:19 -0500

    Implemented hidden conversion
    implemented the hidden conversion to cups for every ingredient entered.
    
    set favicon on base template

--- reciplan/urls.py
+++ reciplan/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("register/", views.register, name="register"),
     path("search/", views.search, name="search"),
-    path("recipe/(?<name>\*)", views.detail, name="detail"),
+    path("recipe/(?<id>\*)", views.detail, name="detail"),
     path("reciplan/home", views.home, name='home'),
 ]
