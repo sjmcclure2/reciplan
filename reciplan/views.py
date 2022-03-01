2d2cc63a765709145df1b9a0a494c205435c69bc
jd1224 <Joshua.coe11@yahoo.com>
2022-02-28 19:54:07 -0500

    Updated search algorithm
    Updated search algorith to search both ingredients and titles - views.py
    removed previous changes in dropdown - search.html
    added in search for both ingredient and title functionality - search.html

--- reciplan/views.py
+++ reciplan/views.py
@@ -12,6 +12,7 @@
 #            1.5 - JC - Updated the conversion implementation
 #            1.6 - JC - Updated search algorithm to allow search by ingredient
 #            1.7 - DH - Added directions to if-else renders
+#            1.8 - JC - Updated search algorith to search both ingredients and titles
 
 from distutils import errors
 from sre_constants import IN
@@ -121,23 +122,18 @@ def search(request):
     if request.method == "POST":
         query_name = request.POST.get('name')
         if query_name:
-            if request.POST.get('search_category') == 'Recipe Title':
-                #query the DB for recipes with a title containing the search term
-                results = Recipe.objects.filter(title__icontains=query_name)
-            elif request.POST.get('search_category') == 'Ingredients':
-                #query the DB for ingredients that match the query name
-                ingredients = Ingredients.objects.filter(name__contains=query_name)
-                names = []
-                #get all recipe names containg the ingredient
-                for i in ingredients:
-                    names.append(i.recipe)
-                results = []
-                #get all recipe objects in query sets
-                for j in names:
-                    set = Recipe.objects.filter(title=j)
-                    #add each recipe from the query set into the list
-                    for k in set:
-                        results.append(k)
+            #create the results array
+            results = []
+            #query the DB for ingredients that match the query name
+            ingredients = Ingredients.objects.filter(name__contains=query_name)
+            #add all recipes containing ingredients to the results
+            for i in ingredients:
+                results.append(i.recipe)
+            #find recipes with a title containing the query name
+            titles = Recipe.objects.filter(title__icontains=query_name)
+            #add recipes from the title search to the results
+            for L in titles:
+                results.append(L)
             #return the search template with the required variables for the display
             return render(request, 'reciplan/search.html', {"results":results, "query":query_name, 'recipes':recipes})
     #if there are no results display all available recipes in a list.
